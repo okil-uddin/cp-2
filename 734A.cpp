b62f65127902d4b132ca3c29ed28ee5a8c8562cd
Okil Uddin <okiluddin1999@gmail.com>
2026-07-05 21:08:50 +0600

    734A

---
+++ 734A.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    string x;
+    cin >> x;
+    int a = 0;
+    int d = 0;
+    for (int i = 0; i < n; i++){
+        if(x[i]=='A'){
+            a++;
+        }
+        else{
+            d++;
+        }
+    }
+    if(a>d){
+        cout<<"Anton"<<endl;
+    }
+    else if(d>a){
+        cout<<"Danik"<<endl;
+    }
+    else{
+        cout<<"Friendship"<<endl;
+    }
+    return 0;
+}
