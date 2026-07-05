c5ae6aec6628a7478552d80640256c29f87d57ac
Okil Uddin <okiluddin1999@gmail.com>
2026-07-05 19:26:37 +0600

    2218A

---
+++ 2218A.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    while(n--){
+        int x;
+        cin>>x;
+        if(x==67){
+            cout<<67<<'\n';
+        }
+        else {
+            cout<<x+1<<'\n';
+        }
+    }
+    return 0;
+}
